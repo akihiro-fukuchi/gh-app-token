112d05d3e0b6731eec7da51a7c3136158c7b2261
akihiro-fukuchi <akihiro.fukuchi@gmail.com>
2022-07-17 13:20:02 +0000

    add flag

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+var (
+	appId          = flag.String("app-id", "", "GitHub app id")
+	installationId = flag.String("installation-id", "", "GitHub app installation id")
+	privateKey     = flag.String("private-key", "", "Gitub app private key")
+
+	apiBaseURL = "https://api.github.com"
+)
+
+func main() {
+	flag.Parse()
+	if *appId == "" || *installationId == "" || *privateKey == "" {
+		flag.PrintDefaults()
+		exitError(fmt.Errorf("missing flags"))
+	}
+	fmt.Print(apiBaseURL)
+}
+
+func exitError(err error) {
+	fmt.Fprintf(os.Stderr, "ERROR: %+v\n", err)
+	os.Exit(1)
 }
